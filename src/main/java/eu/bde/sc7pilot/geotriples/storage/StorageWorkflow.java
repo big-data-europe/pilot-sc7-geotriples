544900359dd87b649f177460b5846f1d8de9e10f
Efi <efi.karrat@gmail.com>
2016-07-14 18:30:10 +0300

    strabon added

---
+++ src/main/java/eu/bde/sc7pilot/geotriples/storage/StorageWorkflow.java
@@ -0,0 +1,57 @@
+package eu.bde.sc7pilot.geotriples.storage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+import eu.bde.sc7pilot.geotriples.GeotriplesConverter;
+import eu.bde.sc7pilot.geotriples.model.Change;
+import eu.bde.sc7pilot.geotriples.model.Event;
+
+public class StorageWorkflow {
+	private String outputDirectory="/resources/";
+	private String STRABON_HOST="localhost";
+	private int STRABON_PORT=5432;
+	private String DB_NAME="events-changes";
+	private String PASSWORD="postgres";
+	private String USERNAME="postgres";
+	public synchronized void  storeChanges(List<Change> changes) throws Exception {
+		ObjectMapper objectMapper=new ObjectMapper().registerModule(new JtsModule()).registerModule(new JodaModule());
+		String mappingFileName="change-mapping.ttl";
+		String jsonFileName="changes.json";
+		String rdfFileName="changes.nt";
+		
+		try {
+			objectMapper.writer().writeValue(new File(outputDirectory,jsonFileName), changes);
+		} catch (IOException e) {
+			throw e;
+		}
+		store(mappingFileName,rdfFileName,jsonFileName);
+	}
+	
+	public synchronized void storeEvent(Event event) throws Exception{
+		ObjectMapper objectMapper=new ObjectMapper().registerModule(new JtsModule()).registerModule(new JodaModule());
+		String mappingFileName="event-mapping.ttl";
+		String jsonFileName="events.json";
+		String rdfFileName="events.nt";
+		
+		try {
+			objectMapper.writer().writeValue(new File(outputDirectory,jsonFileName), event);
+		} catch (IOException e) {
+			throw e;
+		}
+		store(mappingFileName,rdfFileName,jsonFileName);
+	}
+	private void store(String mappingFileName,String rdfFileName,String jsonFileName) throws Exception{
+		GeotriplesConverter.tempGeotriples(outputDirectory+mappingFileName, outputDirectory+rdfFileName, outputDirectory+jsonFileName);
+		RdfStorage rdfStorage=new StrabonRdfStorage(DB_NAME, USERNAME, PASSWORD, STRABON_PORT, STRABON_HOST);
+		rdfStorage.storeRdf(outputDirectory+rdfFileName);
+		new File(outputDirectory,jsonFileName).delete();
+		new File(outputDirectory,rdfFileName).delete();
+	}
+	
+}
---
+++ src/main/java/eu/bde/sc7pilot/geotriples/storage/StrabonRdfStorage.java
@@ -0,0 +1,88 @@
+package eu.bde.sc7pilot.geotriples.storage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import eu.earthobservatory.runtime.postgis.Strabon;
+import eu.earthobservatory.utils.Format;
+
+public class StrabonRdfStorage implements RdfStorage{
+	boolean closed = true;
+	private Strabon strabon = null;
+	private String db;
+	private String username;
+	private String password;
+	private int port;
+	private String host;
+	public StrabonRdfStorage(String db,String username,String password,int port,String host) {
+		this.db=db;
+		this.username=username;
+		this.password=password;
+		this.port=port;
+		this.host=host;
+	}
+	public void storeRdf(String rdfPath) throws Exception{
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				if (strabon != null && !closed) {
+					strabon.close();
+				}
+			}
+		});
+		try {
+			try {
+				strabon = new Strabon(db, username, password, port, host, true);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			strabon.storeInRepo(rdfPath, "NTRIPLES", false);
+			// String[]
+			// argrs={"localhost","5432","endpoint","postgres","postgres",rdfPath,"-f","
+			// NTRIPLES"};
+			// StoreOp.main(argrs);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (strabon != null) {
+				closed = true;
+				strabon.close();
+			}
+		}
+	}
+	@Override
+	public String queryRdf(String query) throws Exception {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				if (strabon != null && !closed) {
+					strabon.close();
+				}
+			}
+		});
+		try {
+			Format resFormat=Format.XML;
+			OutputStream stream=new ByteArrayOutputStream();
+			try {
+				strabon = new Strabon(db, username, password, port, host, true);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			strabon.query(query, resFormat,stream);
+			System.out.println(stream.toString());
+			return stream.toString();
+			// String[]
+			// argrs={"localhost","5432","endpoint","postgres","postgres",rdfPath,"-f","
+			// NTRIPLES"};
+			// StoreOp.main(argrs);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (strabon != null) {
+				closed = true;
+				strabon.close();
+			}
+		}
+		
+	}
+}
